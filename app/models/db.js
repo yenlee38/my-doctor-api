e6f460aec6855a62e72fc3ed75ea9c4eefcb2b50
yenlee38 <ngocyen174308@gmail.com>
2021-08-12 15:08:51 +0700

    change database

--- app/models/db.js
+++ app/models/db.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 require('dotenv').config();
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
