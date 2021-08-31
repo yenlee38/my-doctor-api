b10c006db9697fc7b63a26d2c9e861436666c527
yenlee38 <ngocyen174308@gmail.com>
2021-08-31 23:41:12 +0700

    change method

--- app/routes/account.routes.js
+++ app/routes/account.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const accounts = require("../controllers/account.controller.js");
   const {requireSignin} = require("../controllers/account.controller.js")
   app.post("/accounts", accounts.create);
-  app.get("/accounts/signin", accounts.signin);
+  app.post("/accounts/signin", accounts.signin);
   app.get("/accounts/signout", accounts.signout);
 
   // Update a Account with accountId
