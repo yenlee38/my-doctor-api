2ad42b75f6937b2b3a0f7ba25e45f466282245fe
yenlee38 <ngocyen174308@gmail.com>
2021-12-24 00:20:30 +0700

    create message

--- app/models/message.model.js
+++ app/models/message.model.js
@@ -9,5 +9,60 @@ const Message = function(message){
     this.isHidden = message.isHidden;
 }
 
+Message.getByRecieverId = (recieverId, result) =>{
+    sql.query(`select * from message where recieverId = "${recieverId}"`, (err, res) =>{
+        if(err){
+            result(err, null);
+            return;
+        }
+
+        if(res.length){
+            result(null, res);
+            return;
+        }
+
+        result({kind: "not_found"}, null);
+    })
+}
+
+Message.getBySenderId = (senderId, result) =>{
+    sql.query(`select * from message where senderId = "${senderId}"`, (err, res) =>{
+        if(err){
+            result(err, null);
+            return;
+        }
+
+        if(res.length){
+            result(null, res);
+            return;
+        }
+
+        result({kind: "not_found"}, null);
+    })
+}
+
+Message.create = ( message,result) =>{
+    sql.query("Insert into message set ?", message, (err, res) =>{
+        if(err){
+            result(err, null);
+            return;
+        }
+        result(null, {...message});
+
+    } )
+}
+
+
+Message.getAll = (result) => {
+    sql.query(`SELECT * FROM message`, (err, res) => {
+      if (err) {
+        result(null, err);
+        return;
+      }
+      result(null, res);
+    });
+  };
+
+  
 module.exports = Message;
 
