d188144f60777837f76727d5a24dc237ee94e613
yenlee38 <ngocyen174308@gmail.com>
2022-01-13 09:37:07 +0700

    clear

--- app/routes/heart-beat.routes.js
+++ app/routes/heart-beat.routes.js
@@ -1,13 +1,13 @@
 module.exports = app =>{
     const heartBeat = require("../controllers/heart-beat.controller.js");
-    const {requireSignin} = require("../controllers/account.controller.js")
+    const {requireSignin} = require("../controllers/account.controller.js");
     
     app.post("/heartBeat", requireSignin, heartBeat.create);
     app.put("/heartBeat/:heartBeatId/delete",requireSignin, heartBeat.delete);
     app.put("/heartBeat/delete/all",requireSignin, heartBeat.deleteAll);
     app.put("/heartBeat/:patientId/delete/patient", requireSignin, heartBeat.deleteAllByPatientId);
-    app.put("/heartBeat/:heartBeatId",requireSignin, heartBeat.update)
-    app.get("/heartBeat", heartBeat.findAll)
-    app.get("/heartBeat/:heartBeatId", heartBeat.findOne)
-    app.get("/heartBeat/:patientId/patient", requireSignin, heartBeat.findByPaitentId)
+    app.put("/heartBeat/:heartBeatId",requireSignin, heartBeat.update);
+    app.get("/heartBeat", heartBeat.findAll);
+    app.get("/heartBeat/:heartBeatId", heartBeat.findOne);
+    app.get("/heartBeat/:patientId/patient", requireSignin, heartBeat.findByPaitentId);
 }
